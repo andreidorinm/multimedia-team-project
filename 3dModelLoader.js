d162587c6a7ef14ca0c5d14863d1c3b65e93be84
andreidorinm <andrei.dorin09@yahoo.com>
2022-12-24 03:01:19 +0200

    fixed three.js

---
+++ 3dModelLoader.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+export function modelLoader() {
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000000);
+
+  //camera that looks at our object
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(5, 5, 2.5);
+
+  // nice ambient light
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+
+  // controls for orbiting around object
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', () => renderer.render(scene, camera));
+  controls.target.set(0, 0, 0);
+  controls.update();
+
+  //load the gltf model
+  const loader = new GLTFLoader();
+  loader.load(
+    'models/alfa.glb',
+    (gltf) => {
+      const model = gltf.scene;
+      model.position.set(0, 0, 0);
+      scene.add(model);
+      renderer.render(scene, camera);
+    },
+    undefined,
+    function (error) {
+      console.error(error);
+    }
+  );
+}
