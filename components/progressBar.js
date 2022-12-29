c7a7eae3ae873f9cabca72b34ec29eebe60251b0
andreidorinm <andrei.dorin09@yahoo.com>
2022-12-30 01:00:49 +0200

    changes

--- components/progressBar.js
+++ components/progressBar.js
@@ -1,10 +1,12 @@
 export default function progressBar() {
   let i = 0;
+  let audio = new Audio('assets/sounds/win98.mp3');
+  audio.play();
   if (i == 0) {
     i = 1;
-    let elem = document.getElementById('my-bar');
+    let elem = document.getElementById('my-progress');
     let width = 1;
-    let id = setInterval(frame, 10);
+    let id = setInterval(frame, 80);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 import './style.scss';
+import './node_modules/aesthetic-css/aesthetic.css';
 import modelLoader from './loaders/modelLoader.js';
 import progressBar from './components/progressBar.js';
 import elemHandler from './handlers/elemHandler';
 
 document.querySelector('.container-progress-bar').innerHTML = `
-    <div class="my-progress">
+<div class="aesthetic-windows-95-loader">
+    <div id="my-progress">
       <div id="my-bar"></div>
     </div>
+  </div>
+  <div class="aesthetic-windows-95-button">
     <div id="btn-click-me">
      <span class="btn-click-me-text">hai la cafelutza</span>
     </div>
+  </div>
+
 `;
 
 document.querySelector('#threejs').innerHTML = `
@@ -17,6 +23,6 @@ document.querySelector('#threejs').innerHTML = `
 `;
 
 //add an event for barmove
-document.querySelector('#btn-click-me').addEventListener('click', elemHandler);
+document.querySelector('#btn-click-me').addEventListener('click', progressBar);
 
 modelLoader();
